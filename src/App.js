2b3d3cfd6c52a0854f0fe649295feb20ad5cbd83
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-26 13:34:36 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import { addItem } from  './actions/items';
- 
+
 class App extends Component {
- 
-  handleOnClick = event => {
-    this.props.addItem()
+
+  handleOnClick() {
+    this.props.addItem();
   }
- 
+
   render() {
     debugger
     return (
       <div className="App">
-        <button onClick={this.handleOnClick}>
+        <button onClick={(event) => this.handleOnClick(event)}>
           Click
           </button>
         <p>{this.props.items.length}</p>
@@ -21,11 +21,17 @@ class App extends Component {
     );
   }
 };
- 
+
 const mapStateToProps = (state) => {
   return {
     items: state.items
   };
 };
- 
-export default connect(state => ({ items: state.items }), { addItem })(App);
+
+const matchDispatchToProps = dispatch => ({
+    addItem: () => {
+      dispatch(addItem())
+    }
+  })
+
+export default connect(mapStateToProps, matchDispatchToProps)(App);
